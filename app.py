fbafdc2c1e4e2f360ce42cfe5f75a2fdd2244a3b
Anthony <anthony.mula.pro@gmail.com>
2026-07-29 15:47:04 +0200

    feat: connect HTML form with Flask backend

--- app.py
+++ app.py
@@ -1,19 +1,27 @@
-from flask import Flask, render_template
-from Backend.link_analyzer import LinkAnalyzer
+from flask import Flask, render_template, request
+from backend.link_analyzer import LinkAnalyzer
 import os
 
 app = Flask(__name__,
-            template_folder='Frontend/templates',
-            static_folder='Frontend/static')
+            template_folder='frontend/templates',
+            static_folder='frontend/static')
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 json_path = os.path.join(BASE_DIR, 'data', 'lexicon.json')
 
 analyzer = LinkAnalyzer(json_path)
 
-# Route principale (page d'accueil)
-@app.route('/')
+# Route principal
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    result = None
+    if request.method == 'POST':
+        user_text = request.form.get('text_input')
+        if user_text:
+            result = analyzer.analyze(user_text)
+
+    return render_template('index.html', result=result)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
